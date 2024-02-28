89ec7972b52f7f9f53039ff60f48cec34d1d1c26
Natalia <kohen.natalia@gmail.com>
2024-02-27 21:46:15 -0300

    creacion carpetas

---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+
+
+const Form = () => {
+  return (
+    <form>
+      <label ></label>
+      <input type="text" 
+      // value={}
+      />
+      <label ></label>
+      <input type="text" />
+      <label ></label>
+      <input type="text"  />
+      <button>Enviar</button>
+    </form>
+  )
+}
+
+export default Form
